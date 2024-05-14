3d39cb827141a5b911d74c42d3b2a76838239efd
Quaaludees <quaalud@gmail.com>
2024-05-14 15:47:33 +0300

    HW - 2 (fix rew)

--- 5-map/app.js
+++ 5-map/app.js
@@ -12,10 +12,10 @@ class HrenMap {
     }
     get(key) {
         const hash = this.getHash(key);
-        // if (!this.stateMap[hash]) {
-        //     return undefined;
-        // }
-        return this.stateMap?.[hash];
+        if (!this.stateMap[hash]) {
+            return undefined;
+        }
+        return this.stateMap[hash];
     }
     has(key) {
         const hash = this.getHash(key);
@@ -46,14 +46,14 @@ class HrenMap {
         return hash.toString();
     }
 }
-const test = new HrenMap([
-    ["огурец", 500],
-    ["помидор", 350],
-    ["лук", 50]
-]);
+// const test = new HrenMap([
+//     ["огурец", 500],
+//     ["помидор", 350],
+//     ["лук", 50]
+// ])
 // console.log(test)
 // console.log(test.has('огурец'))
-console.log(test.get('огурец2'));
+// console.log(test.get('огурец2'))
 // console.log(test.get('помидор'))
 // test.set('помидор', 500);
 // console.log(test.get('помидор'))
@@ -66,3 +66,82 @@ console.log(test.get('огурец2'));
 //
 // test.set(test2, test2);
 // console.log(test.get(test2));
+class HashMap {
+    constructor(initialCapacity = 10) {
+        this.buckets = new Array(initialCapacity);
+        this.collisions = 0;
+        this.capacity = initialCapacity;
+    }
+    set(key, value) {
+        const bucketIndex = this.getIndex(key);
+        if (this.buckets[bucketIndex]) {
+            const findInd = this.buckets[bucketIndex].findIndex(el => el.key === key);
+            if (findInd >= 0) {
+                this.buckets[bucketIndex][findInd] = { key, value };
+                console.log(findInd);
+                return this;
+            }
+            this.buckets[bucketIndex].push({ key, value });
+            if (this.buckets[bucketIndex].length > 1) {
+                this.collisions++;
+            }
+        }
+        else {
+            this.buckets[bucketIndex] = [{ key, value }];
+        }
+        return this;
+    }
+    get(key) {
+        const bucketIndex = this.getIndex(key);
+        for (let arrayIndex = 0; arrayIndex < this.buckets[bucketIndex].length; arrayIndex++) {
+            const entry = this.buckets[bucketIndex][arrayIndex];
+            if (entry.key === key) {
+                return entry.value;
+            }
+        }
+    }
+    delete(key) {
+        const index = this.getIndex(key);
+        const bucket = this.buckets[index];
+        if (!bucket) {
+            return;
+        }
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i].key === key) {
+                if (bucket.length > 1) {
+                    this.collisions = this.collisions - 1;
+                }
+                bucket.splice(i, 1);
+                return;
+            }
+        }
+    }
+    clearAll() {
+        this.buckets = new Array(this.capacity);
+        this.collisions = 0;
+    }
+    hash(key) {
+        let hashValue = 0;
+        const stringTypeKey = `${key}${typeof key}`;
+        for (let index = 0; index < stringTypeKey.length; index++) {
+            const charCode = stringTypeKey.charCodeAt(index);
+            hashValue += charCode << (index * 8);
+        }
+        return hashValue;
+    }
+    getIndex(key) {
+        const indexHash = this.hash(key);
+        return indexHash % this.buckets.length;
+    }
+}
+const hashMap = new HashMap();
+hashMap.set('kotina', 81);
+hashMap.set('psina', 1);
+hashMap.set('strelka', 3);
+hashMap.set('belka', 38);
+hashMap.set('koshka', 28);
+hashMap.set('sobaka', 18);
+console.log(hashMap);
+hashMap.delete('sobaka');
+hashMap.delete('koshka');
+console.log(hashMap);
--- 5-map/app.ts
+++ 5-map/app.ts
@@ -1,78 +1,87 @@
+
 class HrenMap {
-    private stateMap: Record<string, unknown> = {};
+    private buckets: Array<{key: string, value: unknown}[]>;
+    private collisions: number;
+    private readonly capacity: number;
 
-    constructor(initialDate: [unknown, unknown][]) {
-        initialDate.forEach(([key, value]) => {
-            this.set(key, value);
-        })
-
+    constructor(initialCapacity = 10) {
+        this.buckets = new Array(initialCapacity);
+        this.collisions = 0;
+        this.capacity = initialCapacity;
     }
 
-    set(key: unknown, value: unknown): void {
-        const hash = this.getHash(key);
-        this.stateMap[hash] = value;
-    }
-
-    get(key: unknown): unknown | undefined {
-        const hash = this.getHash(key);
-        if (!this.stateMap[hash]) {
-            return undefined;
+    set(key: string, value: unknown) {
+        const bucketIndex = this.getIndex(key);
+        if (this.buckets[bucketIndex]) {
+            const findInd = this.buckets[bucketIndex].findIndex(el => el.key === key)
+            if (findInd >= 0) {
+                this.buckets[bucketIndex][findInd] = {key, value}
+                return this;
+            }
+            this.buckets[bucketIndex].push({ key, value });
+            if (this.buckets[bucketIndex].length > 1) { this.collisions++; }
+        } else {
+            this.buckets[bucketIndex] = [{ key, value }];
         }
-        return this.stateMap[hash];
+        return this;
     }
 
-    has(key: unknown): boolean {
-        const hash = this.getHash(key);
-        if (this.stateMap[hash]) {
-            return true;
+    get(key: string) {
+        const bucketIndex = this.getIndex(key);
+        for (let arrayIndex = 0; arrayIndex < this.buckets[bucketIndex].length; arrayIndex++) {
+            const entry = this.buckets[bucketIndex][arrayIndex];
+            if (entry.key === key) {
+                return entry.value;
+            }
         }
-        return false;
     }
-
-    delete(key: unknown): boolean {
-        const hash = this.getHash(key);
-        if (this.stateMap[hash]) {
-            delete this.stateMap[hash];
-            return true;
+    delete(key: string) {
+        const index = this.getIndex(key);
+        const bucket = this.buckets[index];
+        if (!bucket) {
+            return;
         }
-        return false;
-    }
-
-    clear() {
-        this.stateMap = {};
-    }
-
-    private getHash(key: unknown): string {
-        let hash = 0;
-        const keyToString = JSON.stringify(key);
-        for (let i = 0; i < keyToString.length; i++) {
-            const chr = keyToString.charCodeAt(i);
-            hash = ((hash << 5) - hash) + chr;
-            hash |= 0;
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i].key === key) {
+                if (bucket.length > 1) {
+                    this.collisions = this.collisions - 1;
+                }
+                bucket.splice(i, 1);
+                return;
+            }
         }
-        return hash.toString();
+    }
+    clearAll() {
+        this.buckets = new Array(this.capacity);
+        this.collisions = 0;
+    }
+    private hash(key: string) {
+        let hashValue = 0;
+        const stringTypeKey = `${key}${typeof key}`;
+
+        for (let index = 0; index < stringTypeKey.length; index++) {
+            const charCode = stringTypeKey.charCodeAt(index);
+            hashValue += charCode << (index * 8);
+        }
+        return hashValue;
+    }
+    private getIndex(key: string) {
+        const indexHash = this.hash(key);
+        return indexHash % this.buckets.length;
     }
 }
 
-// const test = new HrenMap([
-//     ["огурец", 500],
-//     ["помидор", 350],
-//     ["лук", 50]
-// ])
+const hashMap = new HrenMap();
 
-// console.log(test)
-// console.log(test.has('огурец'))
-// console.log(test.get('огурец2'))
-// console.log(test.get('помидор'))
-// test.set('помидор', 500);
-// console.log(test.get('помидор'))
-// test.set('помидор222', 500);
-// console.log(test.get('помидор222'))
-// console.log(test.delete('помидор222'))
+hashMap.set('kotina', 81);
+hashMap.set('psina', 1);
+hashMap.set('strelka', 3);
+hashMap.set('belka', 38);
+hashMap.set('koshka', 28);
+hashMap.set('sobaka', 18);
+console.log(hashMap)
+hashMap.delete('sobaka');
+hashMap.delete('koshka');
+console.log(hashMap)
 
-// const test2 = {
-//     vasa: 'luk'
-// }
-//
-// test.set(test2, test2);
-// console.log(test.get(test2));
+
