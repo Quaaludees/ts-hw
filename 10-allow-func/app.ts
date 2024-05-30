9c067cfcbd5f9b6f4e3d718d48f87efdb1b4de34
Quaaludees <quaalud@gmail.com>
2024-05-30 17:24:17 +0300

    HW-4 (decorator for class setter)

---
+++ 10-allow-func/app.ts
@@ -0,0 +1,31 @@
+class User {
+
+    @allowFunc((a: number) => a > 0)
+    age: number = 40;
+}
+
+function allowFunc(fn: (a: number) => boolean) {
+    return function(target: Object, propertyKey: string | symbol) {
+        let value: number;
+        const set = function(newValue: number) {
+            if (fn(newValue)) {
+                value = newValue;
+            } else {
+                console.log(`Uhodi`)
+            }
+        }
+        const get = function() {
+            return value;
+        }
+        Object.defineProperty(target, propertyKey, {
+            set: set,
+            get: get
+        })
+    }
+}
+
+const user = new User();
+user.age = -50;
+console.log(user.age);
+user.age = 50;
+console.log(user.age);
